f308449340a5d13e7601419c765a52f511728df1
Jessica Au <75815677+jessica-au@users.noreply.github.com>
2021-06-24 15:47:19 -0700

    Update client/src/components/ActiveChat/SenderBubble.js
    Cleanup for ternary
    
    Co-authored-by: ThinkTink <50761589+ThinkTink@users.noreply.github.com>

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -41,7 +41,7 @@ const SenderBubble = (props) => {
       <Box className={classes.bubble}>
         <Typography className={classes.text}>{text}</Typography>
       </Box>
-        {isLastReadMessage ? <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.lastSeenAvatar}></Avatar> : null }
+        {isLastReadMessage && <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.lastSeenAvatar}></Avatar> }
     </Box>
   );
 };
