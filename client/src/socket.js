4b245650a49fbf5e52574d75765699a6f3a55156
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-06-25 13:15:35 -0700

    Add token auth/handshake logic for socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -6,7 +6,11 @@ import {
   addOnlineUser,
 } from "./store/conversations";
 
-const socket = io(window.location.origin);
+const socket = io(window.location.origin, {
+  auth: {
+    token: "token",
+  }
+});
 
 socket.on("connect", () => {
   console.log("connected to server");
