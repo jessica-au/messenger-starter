7ea30092a954d02eaa42229098430a837c58094b
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-06-28 15:18:13 -0700

    Address comments about material UI component switches and SVG

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 import { loginSignupStyles as useStyles } from "../themes/theme";
+import Bubble from "../assets/bubble.svg"
 
 const Hero = () => {
     const classes = useStyles();
@@ -8,13 +9,13 @@ const Hero = () => {
     return (
         <Box className={classes.bgContainer}>
             <Box className={classes.image}>
-                <div className={classes.textWrapper}>
-                    <h1 className={classes.text}>
-                    <i class="far fa-comment-dots" style={{fontSize: "70px", marginBottom: 20}}/>
+                <Box className={classes.textWrapper}>
+                    <Typography className={classes.text} variant="h4">
+                    <img alt="messageBubble" src={Bubble}/>
                         <br></br>
                         Converse with anyone with any language
-                    </h1>
-                </div>
+                    </Typography>
+                </Box>
                 <img className={classes.image} alt="logo"
                     src="https://res.cloudinary.com/dc5yjx4v5/image/upload/v1624581061/bg-img_ouep5g.png">
                 </img>
